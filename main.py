67026a2ae7b260733042869338d4115b88c04e7c
ckellum <cjkellum@gmail.com>
2023-02-20 12:44:55 -0700

    Load excel files into dataframes

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pandas as pd
+import glob
 from fpdf import FPDF
 
-df1 = pd.read_excel("10001-2023.1.18.xlsx")
-df2 = pd.read_excel("10002-2023.1.18.xlsx")
-df3 = pd.read_excel("10003-2023.1.18.xlsx")
+filepaths = glob.glob("invoices/*.xlsx")
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
 
-print(df3)
+
+print(filepaths)
+
+
+
+
