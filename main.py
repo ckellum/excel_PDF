bbddd4fb37386a6ac752a11d241f7d45f02b113f
ckellum <cjkellum@gmail.com>
2023-02-20 12:32:19 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from fpdf import FPDF
+
+df1 = pd.read_excel("10001-2023.1.18.xlsx")
+df2 = pd.read_excel("10002-2023.1.18.xlsx")
+df3 = pd.read_excel("10003-2023.1.18.xlsx")
+
+print(df3)
